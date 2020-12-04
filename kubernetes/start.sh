65c045ebe5b4f826cd76956fd26c03a75dbee630
候帅 <houshuai0816@126.com>
2020-12-04 11:30:03 +0800

    完善并修改对应的模块内容

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -2,6 +2,6 @@ kubectl create -f flink-configuration-configmap.yaml
 kubectl create -f jobmanager-service.yaml
 kubectl create -f jobmanager-rest-service.yaml
 kubectl create -f taskmanager-query-state-service.yaml
-kubectl create -f jobmanager-job.yaml
+kubectl create -f jobmanager-session-deployment.yaml
 sleep 1
-kubectl create -f taskmanager-job-deployment.yaml
+kubectl create -f taskmanager-session-deployment.yaml
--- kubernetes/stop.sh
+++ kubernetes/stop.sh
@@ -2,5 +2,5 @@ kubectl delete -f flink-configuration-configmap.yaml
 kubectl delete -f jobmanager-service.yaml
 kubectl delete -f jobmanager-rest-service.yaml
 kubectl delete -f taskmanager-query-state-service.yaml
-kubectl delete -f jobmanager-job.yaml
-kubectl delete -f taskmanager-job-deployment.yaml
+kubectl delete -f jobmanager-session-deployment.yaml
+kubectl delete -f taskmanager-session-deployment.yaml
